5f34f9c01e60a0b2bbc0fbbb5468d00ed566d2e7
collapsinghierarchy <58694234+collapsinghierarchy@users.noreply.github.com>
2023-05-28 07:08:49 +0200

    add signatures to the test-case

--- src/main/java/com/cryptinvent/test/CryptInventTestfile.java
+++ src/main/java/com/cryptinvent/test/CryptInventTestfile.java
@@ -1,4 +1,5 @@
 import javax.crypto.Cipher;
+import java.security.Signature;
 import java.security.NoSuchAlgorithmException;
 import javax.crypto.NoSuchPaddingException;
 import java.io.*;
@@ -9,6 +10,10 @@ public class CryptInventTestfile {
   public void foo() {
     try {
       Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+      Cipher c1 = Cipher.getInstance("RSA/ECB/OAEPWithSHA-1AndMGF1Padding"); 
+      Cipher c2 = Cipher.getInstance("DES/ECB/NoPadding"); 
+    
+      Signature sig = Signature.getInstance("SHA256withRSA"); 
     } catch(NoSuchAlgorithmException e) {
       System.out.println("Something is wrong");
     } catch(NoSuchPaddingException ep) {
